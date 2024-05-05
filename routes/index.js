83d87e779fed9930c0be70bb850752254d642866
u-khang <khang.nguyen8675@gmail.com>
2024-05-05 16:53:42 -0400

    add a README

--- routes/index.js
+++ routes/index.js
@@ -13,8 +13,8 @@ const messages = [
       timestamp: new Date()
     },  
     {
-      text: "a very big message ASDFJAWEFOIAWJEWJE;FASDFJAWEFOIAWJE;FOIAWJEF;IAWJE;FIJWAEFAWEFASDFJAWEFOIAWJE;FOIAWJEF;IAWJE;FIJWAEFAWEFASDFJAWEFOIAWJE;FOIAWJEF;IAWJE;FIJWAEFAWEFASDFJAWEFOIAWJE;FOIAWJEF;IAWJE;FIJWAEFAWEFASDFJAWEFOIAWJE;FOIAWJEF;IAWJE;FIJWAEFAWEFASDFJAWEFOIAWJE;FOIAWJEF;IAWJE;FIJWAEFAWEFASDFJAWEFOIAWJE;FOIAWJEF;IAWJE;FIJWAEFAWEFASDFJAWEFOIAWJE;FOIAWJEF;IAWJE;FIJWAEFAWEFASDFJAWEFOIAWJE;FOIAWJEF;IAWJE;FIJWAEFAWEFASDFJAWEFOIAWJE;FOIAWJEF;IAWJE;FIJWAEFAWEFASDFJAWEFOIAWJE;FOIAWJEF;IAWJE;FIJWAEFAWEFASDFJAWEFOIAWJE;FOIAWJEF;IAWJE;FIJWAEFAWEFASDFJAWEFOIAWJE;FOIAWJEF;IAWJE;FIJWAEFAWEFASDFJAWEFOIAWJE;FOIAWJEF;IAWJE;FIJWAEFAWEFIJWAEFAWEFASDFJAWEFOIAWJE;FOIAWJEF;IAWJE;FIJWAEFAWEFAEFAWEF",
-      username: "Applejack",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      username: "Twilight Sparkle",
       timestamp: new Date()
     },
   ];
