d279b8f039c3a70d312ba333a0cde8023f33a9bb
u-khang <khang.nguyen8675@gmail.com>
2024-05-05 14:09:06 -0400

    cleaning up and finishing all functionalities

--- routes/index.js
+++ routes/index.js
@@ -2,21 +2,35 @@ var express = require('express');
 var router = express.Router();
 
 const messages = [
-  {
-    text: "Hi there!",
-    user: "Amando",
-    added: new Date()
-  },
-  {
-    text: "Hello World!",
-    user: "Charles",
-    added: new Date()
-  }
-];
+    {
+      text: "Hi there!",
+      username: "Amando",
+      timestamp: new Date()
+    },
+    {
+      text: "Hello World!",
+      username: "Charles",
+      timestamp: new Date()
+    }
+  ];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {messages: messages});
 });
+
+router.post('/new-message', function(req, res, next) {
+  let text = req.body.messageText;
+  let username = req.body.username;
+
+  let newMessage = {
+    text: text,
+    username: username,
+    timestamp: new Date(),
+  }
+
+  messages.push(newMessage);
+  res.redirect('/');
+})
 
 module.exports = router;
