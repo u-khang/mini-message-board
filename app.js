e1044db9a27dc19981276c516cd728a5e89c21c1
u-khang <khang.nguyen8675@gmail.com>
2024-05-05 13:34:38 -0400

    finished all routings and view templates

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var pugtestRouter = require('./routes/pugtest');
+var newMessageRouter = require('./routes/new-message');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/pugtest', pugtestRouter);
+app.use('/new-message', newMessageRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
